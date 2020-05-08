1514a89a5720fe912da074de070c4e1961e75604
Runnlin <z77072716565@gmail.com>
2020-05-08 11:16:30 +0800

    First create-Spring part

---
+++ wj/src/main/java/io/github/runnlin/WjApplication.java
@@ -0,0 +1,13 @@
+package io.github.runnlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjApplication.class, args);
+    }
+
+}
---
+++ wj/src/test/java/io/github/runnlin/WjApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.runnlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
